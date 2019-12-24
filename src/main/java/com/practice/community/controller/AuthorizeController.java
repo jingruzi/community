e8ae181534a0a4aeb83eee11682475b20a359f4e
jingruzi <1127513197@qq.com>
2019-12-24 22:00:49 +0800

    back-end join publish fuction

--- src/main/java/com/practice/community/controller/AuthorizeController.java
+++ src/main/java/com/practice/community/controller/AuthorizeController.java
@@ -42,7 +42,7 @@ public class AuthorizeController {
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
         GithubUser githubUser = githubProvider.getUser(accessToken);
-        if(githubUser != null){
+        if(githubUser != null && githubUser.getId() != null){
             User user = new User();
             String token = UUID.randomUUID().toString();
             user.setToken(token);
@@ -58,7 +58,7 @@ public class AuthorizeController {
             return "redirect:/";
         }else{
             //登录失败，重新登陆
-            return  "reirect:/";
+            return  "redirect:/";
         }
     }
 }
