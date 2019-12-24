705371425e948ce9905e07a1fe28784aafe16fed
jingruzi <1127513197@qq.com>
2019-12-24 09:00:34 +0800

    add h2 store user

--- README.md
+++ README.md
@@ -6,7 +6,26 @@
 <br>[es](https://elasticsearch.cn/explore)
 <br>[Github deploy key](https://developer.github.com/v3/guides/managing-deploy-keys/#deploy-keys)
 <br>[Bootstrap](https://v3.bootcss.com/components/#navbar-default)
-[Github OAuth 文档](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+<br>[Github OAuth 文档](https://developer.github.com/apps/building-oauth-apps/creating-an-oauth-app/)
+<br>[okHttp](https://square.github.io/okhttp/)
+<br>[h2](http://www.h2database.com/html/quickstart.html)
+<br>[Spring DataBase Support](https://docs.spring.io/spring-boot/docs/2.0.0.RC1/reference/htmlsingle/#boot-features-embedded-database-support)
+<br>[Mybaties](http://mybatis.org/spring-boot-starter/mybatis-spring-boot-autoconfigure/)
+
 ## 工具
 [Git](https://www.git-scm.com/download/)
 <br>[Visual Paradigm](https://www.visual-paradigm.com)
+<br>[Maven repository](https://mvnrepository.com/)
+
+## 脚本
+```sql
+CREATE CACHED TABLE "PUBLIC"."USER"(
+    "ID" INT DEFAULT NEXT VALUE FOR "PUBLIC" NOT NULL,
+    "ACCOUNT_ID" VARCHAR(100),
+    "NAME" VARCHAR(50),
+    "TOKEN" CHAR(36),
+    "GMT_CREATE" BIGINT,
+    "GMT_MODIFIED" BIGINT
+)
+
+```
--- src/main/java/com/practice/community/controller/AuthorizeController.java
+++ src/main/java/com/practice/community/controller/AuthorizeController.java
@@ -2,6 +2,8 @@ package com.practice.community.controller;
 
 import com.practice.community.dto.AccessTokenDTO;
 import com.practice.community.dto.GithubUser;
+import com.practice.community.mapper.UserMapper;
+import com.practice.community.model.User;
 import com.practice.community.provider.GithubProvider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -9,22 +11,28 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
 //当前类作为路由API的承载者
 @Controller
 public class AuthorizeController {
     @Autowired
     private GithubProvider githubProvider;
-
     @Value("${github.client.id}")
     private String clientId;
     @Value("${github.client.secret}")
     private String clientSecret;
     @Value("${github.redirect.uri}")
     private String redirectUri;
+    
+    @Autowired
+    private UserMapper userMapper;
 
     @GetMapping("/callback")
     public String callback(@RequestParam(name="code") String code,
-                           @RequestParam(name="state") String state){
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request){
         AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
         accessTokenDTO.setClient_id(clientId);
         accessTokenDTO.setClient_secret(clientSecret);
@@ -32,8 +40,21 @@ public class AuthorizeController {
         accessTokenDTO.setRedirect_uri(redirectUri);
         accessTokenDTO.setState(state);
         String accessToken = githubProvider.getAccessToken(accessTokenDTO);
-        GithubUser user = githubProvider.getUser(accessToken);
-        System.out.println(user.getId());
-        return "index";
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if(githubUser != null){
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate());
+            userMapper.insert(user);
+            //登录成功，写cookie和session
+            request.getSession().setAttribute("user",githubUser);
+            return "redirect:/";
+        }else{
+            //登录失败，重新登陆
+            return  "reirect:/";
+        }
     }
 }
